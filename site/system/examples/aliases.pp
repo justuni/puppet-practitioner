e2f91cbb0b03c8e70559f57928bf6715efae7a27
pcuenco <pcuenco@puppetlabs.vm>
2017-12-05 20:53:22 +0000

    Update 'site/system/examples/aliases.pp'

--- site/system/examples/aliases.pp
+++ site/system/examples/aliases.pp
@@ -1,9 +1,17 @@
-user { 'admin':
-  ensure => present,
-}
-
-class { 'system::aliases':
-  admin   => 'admin',
-  require => User['admin'],
+class system::aliases (
+  String $admin = 'root',
+) {
+    file { '/etc/aliases':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => epp('system/aliases.pp', { admin => $admin}),
+    }
+    
+    exec {
+      refreshonly  => true,
+      subscribe    => File['/etc/aliases'],
+    }
 }
 
