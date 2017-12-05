1f56222ecf74f6bf6b5fbd0b24870df5928956e3
james <james@puppetlabs.vm>
2017-12-05 16:57:53 +0000

    Update 'site/review/manifests/init.pp'

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -16,7 +16,7 @@ class review (
     managehome => true,
   }
 
-  file { '/home/${user}/.bashrc':
+  file { "/home/${user}/.bashrc":
     ensure => file,
     owner  => $user,
     group  => $user,
