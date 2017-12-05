8c911a4cb27e46a2f2cce77cb9ea888922e2cc06
pcuenco <pcuenco@puppetlabs.vm>
2017-12-05 20:08:23 +0000

    Update 'site/system/examples/managed_user.pp'

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,6 +1,6 @@
 # Linux requires a hash for the password. This one is 'Puppet8Labs!'
 # Generate your own with the command `openssl passwd -1` if you'd like.
-$password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
+$password = '$1$RusAfONw$pcUVXo39wvJUZptP3BJ9Y.'
 
 system::managed_user { ['aaron', 'kaitlin', 'alison']:
   password => $password,
