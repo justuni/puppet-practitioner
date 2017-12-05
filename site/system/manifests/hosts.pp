0b7009cc56709e8a2368ad7166ecdf1fe84ebaeb
ssiegel <ssiegel@puppetlabs.vm>
2017-12-05 19:36:49 +0000

    Update 'site/system/manifests/hosts.pp'

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,27 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+resources {'host':
+purge => true,
+}
+host { 'master.puppetlabs.vm':
+ensure => present,
+host_aliases => ['master'],
+ip => 54.200.191.96, ## use the classroom IP
+}
+host { 'localhost':
+ensure => present,
+host_aliases => [
+'localhost.localdomain',
+'localhost4',
+'localhost4.localdomain4',
+'training.puppetlabs.vm',
+'training'
+],
+ip => '127.0.0.1',
+}
+## Use your own IP, or the ::ipaddress fact
+host { ssiegel.puppetlabs.vm:
+ensure => present,
+host_aliases => [ssiegel],
+ip => $::ipaddress,
+}
 }
