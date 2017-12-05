d5df153f48d9bec784981556e9f1a0d899388273
faulconj <faulconj@puppetlabs.vm>
2017-12-05 19:34:13 +0000

    Update 'site/system/manifests/hosts.pp'

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,46 @@
 class system::hosts {
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
+  resources {'host':
+    purge => true,
+  }
+
+  host { 'faulconj.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['faulconj'],
+    ip           => '172.17.0.12',
+    target       => '/etc/hosts',
+  }
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+
 }
