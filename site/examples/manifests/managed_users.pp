22aeea64fc927037cfcf262af7522723601a38ee
erosier <erosier@puppetlabs.vm>
2017-12-05 20:33:56 +0000

    Update 'site/examples/manifests/managed_users.pp'

--- site/examples/manifests/managed_users.pp
+++ site/examples/manifests/managed_users.pp
@@ -1,7 +1,3 @@
-# example declarations for the defined type exercise 
-
-$password = '$1$KvlzY/4u$19Oe.k1wNeKhh..uKj6wV1'
-
 class examples::managed_users($password) {
   system::managed_user { ['aaron', 'kaitlin', 'jose']:
     password => $password,
