0a1bc378c4c78f0ecaeccc508bd94c3ff5180aac
ssiegel <ssiegel@puppetlabs.vm>
2017-12-05 17:35:52 +0000

    Update 'site/review/manifests/init.pp'

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,25 +1,25 @@
-class review {
+class review (
 	$user = 'review'
     ){
   # this class should accept a parameter rather than having
   # the username hardcoded.
 
   # Uncomment and use this variable where appropriate
-#  $homedir = $user ? {
-#    'root'  => '/root',
-#    default => "/home/$user",
-#  }
+  $homedir = $user ? {
+    'root'  => '/root',
+    default => "/home/$user",
+  }
 
-  user { '$user':
+  user { $user:
     ensure     => present,
     shell      => '/bin/bash',
     managehome => true,
   }
 
-  file { '/home/bob/.bashrc':
+  file { "$homedir/.bashrc":
     ensure => file,
-    owner  => '$user',
-    group  => '$user',
+    owner  => $user,
+    group  => $user,
     mode   => '0644',
     source => 'puppet:///modules/review/bashrc'
   }
