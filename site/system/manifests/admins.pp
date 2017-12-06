371690666b8a3ab7e07d0c5ca5bb69de317d0c64
james <james@puppetlabs.vm>
2017-12-06 14:40:57 +0000

    Update 'site/system/manifests/admins.pp'

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,23 +1,20 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
+
+  $mysql_users = ['zack@localhost', 'monica@localhost', 'brad@localhost', 'luke@localhost']
+
+  $mysql_users.each |String $mysql_user| {
+    mysql_user { "${mysql_user}":
+      ensure => present,
+      max_queries_per_hour => 600,
+    }
   }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
+
+  mysql_user { 'zack@localhost':
+    max_queries_per_hour => 1200,
   }
   mysql_user { 'ralph@localhost':
     ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
   }
 
   user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
