7e9a653f3522ff29f4f74837e65271e51757e388
erosier <erosier@puppetlabs.vm>
2017-12-06 14:53:23 +0000

    Update 'site/system/manifests/admins.pp'

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -4,20 +4,20 @@ class system::admins {
   $retired = [ 'ralph' ]
   
   $admins = {
-    'brad' => { max_queries_per_hour => '600' },
+    'brad'   => { max_queries_per_hour => '600' },
     'monica' => { max_queries_per_hour => '600' },
-    'luke' => { max_queries_per_hour => '600' },
-    'zack' => { max_queries_per_hour => '1200' },
+    'luke'   => { max_queries_per_hour => '600' },
+    'zack'   => { max_queries_per_hour => '1200' },
   }
 
   $admins.each |$user, $params| {
     mysql_user { "${user}@localhost":
-      ensure => present,
+      ensure 			   => present,
       max_queries_per_hour => $params['max_queries_per_hour'],
     }
 
     user { $user:
-      ensure => present,
+      ensure     => present,
       managehome => true,
     }
   }
