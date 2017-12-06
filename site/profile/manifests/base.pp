fdbeabd8bbbebd4edc462b1c8d449ccb469a9d09
Troy Klein <troy.klein@puppet.com>
2017-12-06 14:48:53 +0000

    changed profiles::base to reflect the base classroom configuration based on the exercises and labs.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,16 @@
 class profile::base {
   notify { "Hello, my name is ${::hostname}": }
+  include review
+  include system::hosts
+
+  class { 'userprefs':
+    shell => 'bash',
+    editor => 'vim',
+  }
+  class { 'examples::managed_users':
+    password => '$1$1Z1jKUQX$zcvkZr13EaqMOq2nkeSHz1',
+  }
+  class { 'system::aliases':
+    admin => 'root',
+  }
 }
